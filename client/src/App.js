88d4682dacafcd9720881ff4b16d912af4d21470
sceka <scekicm02@gmail.com>
2022-01-22 11:46:56 +0100

    Test commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className=''>
+      <h1 className="text-3x1 underline font-bold">Hello World</h1>
+      <h2>Hello there and welcome to my dapp</h2>
+    </div>
+  );
+}
+
+export default App;
